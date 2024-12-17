ac0694f4b983a3a967431616d4a49acd0b3e9e35
Oleksandr Kazan <kazansasha04@gmail.com>
2024-12-17 18:57:27 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,17 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import {
-  Link,
   Routes,
   Route,
-  useParams,
   NavLink,
-  useLocation,
 } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { TabsPage } from './components/TabsPage';
 import { NotFoundPage } from './components/NotFoundPage';
-import { useEffect, useState } from 'react';
-
-const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
 
 const NOT_SELECTED = 'Please select a tab';
 
 export const App = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  const location = useLocation();
-  const [tabContent, setTabContent] = useState('Please select a tab');
-
-  useEffect(() => {
-    if (tabId) {
-      const activeTab = tabs.find(tab => tab.id === tabId);
-      if (activeTab) {
-        setTabContent(activeTab.content);
-      } else {
-        setTabContent(NOT_SELECTED);
-      }
-    }
-  }, [tabId]);
-
   return (
     <div>
       <nav
@@ -70,32 +45,12 @@ export const App = () => {
         <div className="container">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/tabs" element={<TabsPage />} />
-            <Route path="/tabs/:tabId" element={<TabsPage />} />
+            <Route path="/tabs" element={<TabsPage />}>
+              <Route index element={<div>{NOT_SELECTED}</div>} />{' '}
+              <Route path=":tabId" element={<TabsPage />} />{' '}
+            </Route>
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
-
-          {location.pathname.startsWith('/tabs') && (
-            <>
-              <div className="tabs is-boxed">
-                <ul>
-                  {tabs.map(tab => (
-                    <li
-                      key={tab.id}
-                      className={tab.id === tabId ? 'is-active' : ''}
-                    >
-                      <Link to={`/tabs/${tab.id}`} data-cy="Tab">
-                        {tab.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="block" data-cy="TabContent">
-                {tabContent}
-              </div>
-            </>
-          )}
         </div>
       </div>
     </div>
--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -1,3 +1,48 @@
+import { useEffect, useState } from 'react';
+import { Link, Route, Routes, useParams } from 'react-router-dom';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+const NOT_SELECTED = 'Please select a tab';
+
 export const TabsPage = () => {
-  return <h1 className="title">Tabs page</h1>;
+  const { tabId } = useParams<{ tabId: string }>();
+  const [tabContent, setTabContent] = useState(NOT_SELECTED);
+
+  useEffect(() => {
+    if (tabId) {
+      const activeTab = tabs.find(tab => tab.id === tabId);
+      if (activeTab) {
+        setTabContent(activeTab.content);
+      } else {
+        setTabContent(NOT_SELECTED);
+      }
+    }
+  }, [tabId]);
+
+  return (
+    <div>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li key={tab.id} className={tab.id === tabId ? 'is-active' : ''}>
+              <Link to={`/tabs/${tab.id}`} data-cy="Tab">
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <Routes>
+        <Route index element={<div>{NOT_SELECTED}</div>} />
+        <Route path=":tabId" element={<div>{tabContent}</div>} />
+      </Routes>
+    </div>
+  );
 };
